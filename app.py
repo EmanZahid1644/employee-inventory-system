3c084702c9d8f1c80896beb8257b7fdcc86ad0ae
EmanZahid1644 <emanzahid234@gmail.com>
2025-07-31 11:56:41 -0700

    Fix Procfile and redeploy

--- app.py
+++ app.py
@@ -185,3 +185,7 @@ def create_app(config_name='default'):
 if __name__ == '__main__':
     app = create_app()
     app.run(debug=True)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
